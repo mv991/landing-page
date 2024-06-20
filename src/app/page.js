820c9eaf0272919bd6fc8b58bdb6aaf5334815ac
unknown <mshsk@zangoh.com>
2024-06-20 06:38:30 -0700

    commited

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen home" >
+      <div className="w-screen min-h-screen h-fit home" >
           <div className="flex md:flex-row flex-col h-full w-[80%] mx-auto pt-[60px]">
-            <div className="md:w-1/2 w-full flex flex-col   h-full pt-[160px]">
-                <h1 className="text-3xl w-[350px] text-white font-bold">HARBOUR LIGHTS<br/>DE  <span className="text-[#17ABFF]"> GRESOGONO</span></h1>
-                <p className="text-white font-semibold text-xl mt-4 w-[380px] max-w-full">1, 2 & 3 Bedrooms Seaside Apartments in Dubai Maritime City</p>
+            <div className="md:w-1/2 w-full flex flex-col   h-full md:pt-[160px] pt-[60px]">
+                <h1 className="text-5xl md:w-[370px] text-white font-semibold leading-[60px] tracking-[2px]">HARBOUR LIGHTS<br/>DE  <span className="text-[#17ABFF]"> GRESOGONO</span></h1>
+                <p className="text-white font-semibold text-2xl mt-4 md:w-[530px] max-w-full font-poppins">1, 2 & 3 Bedrooms Seaside Apartments in Dubai Maritime City</p>
             </div>
-            <div className="w-1/2 h-full items-center justify-end flex pt-12">
+            <div className="md:w-1/2 w-full h-full items-center md:justify-end flex pt-12">
               <Card/>
             </div>
           </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="card w-[300px] h-[300px] text-white ">
+    <div className="card w-[300px] h-[300px] text-white md:mb-0 mb-12">
       <div className="w-full px-6 h-[80%] py-8">
-        <p className="text-xs font-[300] mb-2">PRICING STARTS FROM</p>
-        <p className="text-4xl font-bold">$ 425,000</p>
-        <p className="text-xl my-3">AED 1.3 Million</p>
-        <button className="text-white  py-4 w-full mx-auto text-xs rounded-md bg-[#17ABFF]">
+        <p className="text-[12px] font-[300] mb-2 tracking-[4px]">PRICING STARTS FROM</p>
+        <p className="text-5xl font-[500]">$ 425,000</p>
+        <p className="text-xl my-3 font-oswald">AED 1.3 Million</p>
+        <button className="text-white  py-4 w-full mx-auto text-[12px] rounded-md mt-2 font-[500] tracking-[2px] bg-[#17ABFF]">
           GET A PRESENTATION
         </button>
         <div></div>
